10e799428a58cf39c8269f83350a41cb6884c4fa
Sertan Canpolat <sertancanpolat@tutanota.com>
2021-07-22 18:25:37 +0300

    Create Paper database service using Firebase Firestore

---
+++ app/src/main/java/dev/sertan/android/paper/data/db/FirestorePaperDbService.kt
@@ -0,0 +1,79 @@
+package dev.sertan.android.paper.data.db
+
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+import dev.sertan.android.paper.data.model.Paper
+import dev.sertan.android.paper.data.util.Response
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.tasks.await
+
+class FirestorePaperDbService : DbService<Paper> {
+    private val collection: CollectionReference by lazy { Firebase.firestore.collection(COLLECTION) }
+
+    companion object {
+        private const val COLLECTION = "papers"
+    }
+
+    override suspend fun create(data: Paper): Response<Paper> {
+        return try {
+            collection.document(data.uid).set(data).await()
+            Response.success(data)
+        } catch (e: Exception) {
+            Response.error(e)
+        }
+    }
+
+    override suspend fun delete(data: Paper): Response<Boolean> {
+        return try {
+            if (getData(data.uid).isError()) throw DbException.DataNotFound
+            collection.document(data.uid).delete().await()
+            Response.success(true)
+        } catch (e: Exception) {
+            Response.error(e)
+        }
+    }
+
+    override suspend fun update(data: Paper): Response<Boolean> {
+        return try {
+            if (getData(data.uid).isError()) throw DbException.DataNotFound
+            collection.document(data.uid).set(data).await()
+            Response.success(true)
+        } catch (e: Exception) {
+            Response.error(e)
+        }
+    }
+
+    override suspend fun getData(uid: String): Response<Paper> {
+        return try {
+            val document = collection.document(uid).get().await()
+            val paper = document.toObject<Paper>() ?: throw DbException.DataNotFound
+            Response.success(paper)
+        } catch (e: Exception) {
+            Response.error(e)
+        }
+    }
+
+    override fun getAllData(userUid: String): Flow<Response<List<Paper>>> {
+        return callbackFlow<Response<List<Paper>>> {
+            val listenerRegistration = collection
+                .whereEqualTo("userUid", userUid)
+                .addSnapshotListener { value, error ->
+                    if (error != null) cancel()
+                    val papers = value?.documents?.map { document -> document.toObject<Paper>()!! }
+                    if (papers.isNullOrEmpty()) cancel()
+                    trySend(Response.success(papers))
+                }
+
+            awaitClose { listenerRegistration.remove() }
+        }.catch {
+            emit(Response.error(DbException.DataNotFound))
+        }
+    }
+
+}
