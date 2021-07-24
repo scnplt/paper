6a9cba2cc4162a1c29ef6390ff25a8bc6c47987e
Sertan Canpolat <sertancanpolat@tutanota.com>
2021-07-24 15:50:28 +0300

    Remove unnecessary dependency injection in tests

--- app/src/androidTest/java/dev/sertan/android/paper/data/auth/FakeAuthService.kt
+++ app/src/androidTest/java/dev/sertan/android/paper/data/auth/FakeAuthService.kt
@@ -8,7 +8,12 @@ class FakeAuthService : AuthService() {
     private var currentUser: User? = null
 
     override suspend fun currentUser(): Response<User> {
-        return Response.success(currentUser)
+        return try {
+            if (currentUser == null) throw AuthException.UserNotFound
+            Response.success(currentUser)
+        } catch (e: Exception) {
+            Response.error(e)
+        }
     }
 
     override suspend fun register(email: String, password: String): Response<Boolean> {
@@ -25,6 +30,8 @@ class FakeAuthService : AuthService() {
 
     override suspend fun logIn(email: String, password: String): Response<Boolean> {
         return try {
+            validateEmail(email)
+            validatePassword(password)
             if (email != user?.email || password != user?.password) throw AuthException.IncorrectInformation
             currentUser = User(email = email, password = password)
             Response.success(true)
