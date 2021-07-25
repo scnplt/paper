d58c69cb1c38789341181e103ad6a026460a8c08
Sertan Canpolat <58915038+scnplt@users.noreply.github.com>
2021-07-25 20:12:19 +0300

    Move all custom exception classes under one class

--- app/src/androidTest/java/dev/sertan/android/paper/data/auth/FakeAuthService.kt
+++ app/src/androidTest/java/dev/sertan/android/paper/data/auth/FakeAuthService.kt
@@ -1,15 +1,16 @@
 package dev.sertan.android.paper.data.auth
 
 import dev.sertan.android.paper.data.model.User
+import dev.sertan.android.paper.data.util.PaperException
 import dev.sertan.android.paper.data.util.Response
 
-class FakeAuthService : AuthService() {
+class FakeAuthService : AuthService {
     private var user: User? = null
     private var currentUser: User? = null
 
     override suspend fun currentUser(): Response<User> {
         return try {
-            if (currentUser == null) throw AuthException.UserNotFound
+            if (currentUser == null) throw PaperException.UserNotFound
             Response.success(currentUser)
         } catch (e: Exception) {
             Response.error(e)
@@ -18,9 +19,7 @@ class FakeAuthService : AuthService() {
 
     override suspend fun register(email: String, password: String): Response<Unit> {
         return try {
-            validateEmail(email)
-            validatePassword(password)
-            if (email == user?.email) throw AuthException.UserAlreadyExists
+            if (email == user?.email) throw PaperException.UserAlreadyExists
             user = User(email = email, password = password)
             Response.success()
         } catch (e: Exception) {
@@ -30,9 +29,7 @@ class FakeAuthService : AuthService() {
 
     override suspend fun logIn(email: String, password: String): Response<Unit> {
         return try {
-            validateEmail(email)
-            validatePassword(password)
-            if (email != user?.email || password != user?.password) throw AuthException.IncorrectInformation
+            if (email != user?.email || password != user?.password) throw PaperException.IncorrectInformation
             currentUser = User(email = email, password = password)
             Response.success()
         } catch (e: Exception) {
@@ -42,7 +39,7 @@ class FakeAuthService : AuthService() {
 
     override suspend fun logOut(): Response<Unit> {
         return try {
-            if (currentUser == null) throw AuthException.UserNotFound
+            if (currentUser == null) throw PaperException.UserNotFound
             currentUser = null
             Response.success()
         } catch (e: Exception) {
@@ -52,7 +49,7 @@ class FakeAuthService : AuthService() {
 
     override suspend fun deleteAccount(): Response<Unit> {
         return try {
-            if (currentUser == null || user == null) throw AuthException.UserNotFound
+            if (currentUser == null || user == null) throw PaperException.UserNotFound
             user = null
             Response.success()
         } catch (e: Exception) {
@@ -62,8 +59,7 @@ class FakeAuthService : AuthService() {
 
     override suspend fun sendResetPasswordMail(email: String): Response<Unit> {
         return try {
-            validateEmail(email)
-            if (email != user?.email) throw AuthException.UserNotFound
+            if (email != user?.email) throw PaperException.UserNotFound
             Response.success()
         } catch (e: Exception) {
             Response.error(e)
