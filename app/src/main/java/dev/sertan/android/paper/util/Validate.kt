feb9481a0e7dba58d005e663ab817e8d17e02048
Sertan Canpolat <sertancanpolat@tutanota.com>
2021-08-20 00:51:18 +0300

    Fix detekt.style.MagicNumber warning

--- app/src/main/java/dev/sertan/android/paper/util/Validate.kt
+++ app/src/main/java/dev/sertan/android/paper/util/Validate.kt
@@ -3,13 +3,14 @@ package dev.sertan.android.paper.util
 import androidx.core.util.PatternsCompat
 
 object Validate {
+    private const val MIN_PASSWORD_LENGTH = 8
 
     fun email(email: String): Boolean {
         return PatternsCompat.EMAIL_ADDRESS.matcher(email).matches()
     }
 
     fun password(password: String): Boolean {
-        return password.length >= 8
+        return password.length >= MIN_PASSWORD_LENGTH
     }
 
 }
